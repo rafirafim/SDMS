8a725b03791067a6d27699bc0361de03e7e9eb3d
rafirahim <rafipattzhi123@gmail.com>
2019-05-14 08:41:16 +0530

    assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {Comp1Component} from './comp1/comp1.component';
+import {Comp2Component} from './comp2/comp2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'reg',
+    component:Comp1Component,
+  }, 
+  {
+    path:'search',
+    component:Comp2Component,
+  }     
 
+  ];
+export const routingcomp=[Comp1Component,Comp2Component]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import {routingcomp} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingcomp,
   ],
   imports: [
     BrowserModule,
